2ab12fa7a25b93046a81511d69007a6f4254446a
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2022-02-03 13:40:13 +0200

    Update Upload_img_1.py

--- openCV/Upload_img_1.py
+++ openCV/Upload_img_1.py
@@ -1,8 +1,19 @@
+# Установка билиотеки через терминал (один из вариантов)
+# pip install opencv-python
+# pip3 install opencv-python
+# python -m pip install opencv-python
+# python3 -m pip install opencv-python
+
+
 import cv2
 
-img = cv2.imread('python_hero.jpg', 1)   # Опции для отображения (-1, 0, 1)
-img = cv2.resize(img, (400, 400))
+img = cv2.imread('python_hero.jpg', 1)         # Режимы отображения (1, -1 ,0)
+img  = cv2.resize(img, (0, 0), fx=0.5, fy=0.5) # Отобразить половину от изначального размера
+# img = cv2.resize(img, (400, 400))            # Отобразить в пикселях
+img = cv2.rotate(img, cv2.cv2.ROTATE_180)      # Повернуть на 180 градусов
+
+cv2.imwrite('new_img.jpg', img)                # Создать новый файл со всеми изменениями
 
 cv2.imshow('Image', img)
-cv2.waitKey(0)                           # Закрыть окно
+cv2.waitKey(0)
 cv2.destroyAllWIndows()
