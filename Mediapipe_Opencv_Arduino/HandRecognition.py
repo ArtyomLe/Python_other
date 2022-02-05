caed3a50c8a7fee687338d8dda955c877825b05a
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2022-02-05 22:39:29 +0200

    Update HandRecognition.py

--- Mediapipe_Opencv_Arduino/HandRecognition.py
+++ Mediapipe_Opencv_Arduino/HandRecognition.py
@@ -51,3 +51,45 @@ while True:
     cv2.imshow('Image', img)
     if cv2.waitKey(1) == ord('q'):
         break
+        
+#============================================ Отправляем в порт =========================================================
+
+import cv2
+import mediapipe as mp
+import serial
+
+camera = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands        # Подключаем алгоритмы поиска рук
+hands = mpHands.Hands()             # Hands обьект руки с настройками
+mpDraw = mp.solutions.drawing_utils # Утилиты для рисования
+
+portNo = "COM9"                      # Порт для подключения ардуино
+uart = serial.Serial(portNo, 9600)   # Скорость передачи данных
+
+while True:
+    good, img = camera.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(imgRGB)    # .process(один кадр) - поиск руки в кадре
+    if results.multi_hand_landmarks:   # .multi_hand_landmarks - получаем точки на руке
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS) # .draw_landmarks - нарисовать по точкам
+            for id, point in enumerate(handLms.landmark): # enumerate() - получить индекс элемента и значение
+                width, height, color = img.shape         # img.shape - узнаём размер кадра
+                width, height = int(point.x * height), int(point.y * width)
+                if id == 8:
+                    cv2.circle(img, (width, height), 15, (225, 0, 225), -1) # Рисуем окружность на выбранной точке 8 (указ. палец)
+                    
+                    # Если точка найдена выводим её через протокол в ардуино
+                    msg = '#' + str(width) + ';'
+                    msg = bytes(str(msg), 'utf-8') # Кодировка текста (8 бит)
+                    uart.write(msg) # Отправляем байты в шину serial
+
+
+
+
+    cv2.imshow('Image', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+#=======================================================================================================================
